d416370b9b6b614815a1d3af2490d52e9cd91328
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-07-14 00:03:23 -0400

    Rename AppModule to MyTakeModule.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { MyTakeModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(MyTakeModule)
   .catch(err => console.error(err));
