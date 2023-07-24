9d250a8eae0d9faebed6f36fc72c4d8f734a4172
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-07-24 15:53:12 -0400

    test material

--- src/my-take/features/login/login.module.ts
+++ src/my-take/features/login/login.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { LoginRoutingModule } from './login-routing.module';
+import { SharedModule } from '@shared/modules/shared.module';
 import { LoginComponent } from './components/login.component';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
 	declarations: [LoginComponent],
-	imports: [CommonModule, LoginRoutingModule],
+	imports: [CommonModule, SharedModule, LoginRoutingModule],
 })
 export class LoginModule {}
