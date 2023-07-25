8b38cd75fb7df1ccde47d7ae4e29b91417a977f9
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-07-24 21:27:53 -0400

    Create a toolbar component.

--- src/my-take/core/core.module.ts
+++ src/my-take/core/core.module.ts
@@ -5,9 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '@shared/modules/shared.module';
 import { CoreComponent } from './components/core.component';
 import { CoreRoutingModule } from './core-routing.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-	declarations: [CoreComponent],
+	declarations: [CoreComponent, ToolbarComponent],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
