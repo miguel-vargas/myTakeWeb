074b3f51c53159105a2098b122c7366282ac826a
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-07-25 19:56:32 -0400

    fix test.

--- src/my-take/core/components/core.component.spec.ts
+++ src/my-take/core/components/core.component.spec.ts
@@ -1,21 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from '@shared/modules/material/material.module';
 import { CoreComponent } from './core.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 describe('CoreComponent', () => {
-  let component: CoreComponent;
-  let fixture: ComponentFixture<CoreComponent>;
+	let component: CoreComponent;
+	let fixture: ComponentFixture<CoreComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CoreComponent]
-    });
-    fixture = TestBed.createComponent(CoreComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [CoreComponent, ToolbarComponent],
+			imports: [MaterialModule, RouterTestingModule],
+		});
+		fixture = TestBed.createComponent(CoreComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/my-take/core/components/toolbar/toolbar.component.spec.ts
+++ src/my-take/core/components/toolbar/toolbar.component.spec.ts
@@ -1,21 +1,26 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MaterialModule } from '@shared/modules/material/material.module';
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
-  let component: ToolbarComponent;
-  let fixture: ComponentFixture<ToolbarComponent>;
+	let component: ToolbarComponent;
+	let fixture: ComponentFixture<ToolbarComponent>;
+	let loader: HarnessLoader;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ToolbarComponent]
-    });
-    fixture = TestBed.createComponent(ToolbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [ToolbarComponent],
+			imports: [MaterialModule],
+		});
+		fixture = TestBed.createComponent(ToolbarComponent);
+		loader = TestbedHarnessEnvironment.loader(fixture);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
