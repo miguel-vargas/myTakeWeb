ee00b1972c95a45193ef010d278de5f1bcbe61de
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-07-28 13:59:07 -0400

    Install NGRX.

--- src/my-take/core/core.module.ts
+++ src/my-take/core/core.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 import { SharedModule } from '@shared/modules/shared.module';
+import { metaReducers, reducers } from '@state/reducers';
 import { CoreComponent } from './components/core.component';
-import { CoreRoutingModule } from './core-routing.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CoreRoutingModule } from './core-routing.module';
 
 @NgModule({
 	declarations: [CoreComponent, ToolbarComponent],
@@ -15,6 +17,9 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 		CommonModule,
 		SharedModule,
 		CoreRoutingModule,
+		StoreModule.forRoot(reducers, {
+			metaReducers,
+		}),
 	],
 	bootstrap: [CoreComponent],
 })
