d22838c9fd2838d8e5b220028b2d5886f04a2be0
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-08-02 22:18:16 -0400

    Add effects.

--- src/my-take/core/core.module.ts
+++ src/my-take/core/core.module.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { SharedModule } from '@shared/modules/shared.module';
+import { AppEffects } from '@state/effects/app.effects';
 import { metaReducers, reducers } from '@state/reducers';
 import { CoreComponent } from './components/core.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
@@ -20,6 +22,7 @@ import { CoreRoutingModule } from './core-routing.module';
 		StoreModule.forRoot(reducers, {
 			metaReducers,
 		}),
+		EffectsModule.forRoot([AppEffects]),
 	],
 	bootstrap: [CoreComponent],
 })
