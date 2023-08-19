5dfaf59cdd4a4c1110d32369e3b04f4d548fa82d
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-08-19 00:47:46 -0400

    regular login logout buttons.

--- src/my-take/shared/modules/material/material.module.ts
+++ src/my-take/shared/modules/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-	exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+	exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatMenuModule],
 })
 export class MaterialModule {}
