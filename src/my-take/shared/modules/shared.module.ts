cbe9a706438f35c7d8f91b4b2f0f9df704f75640
Miguel Vargas <miguel.vargas.dev@outlook.com>
2023-08-20 01:09:22 -0400

    Add take card component, reactive form module to shared module.

--- src/my-take/shared/modules/shared.module.ts
+++ src/my-take/shared/modules/shared.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TakeCardComponent } from '@shared/components/take-card/take-card.component';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
-	exports: [MaterialModule],
+	declarations: [TakeCardComponent],
+	imports: [MaterialModule],
+	exports: [MaterialModule, ReactiveFormsModule, TakeCardComponent],
 })
 export class SharedModule {}
