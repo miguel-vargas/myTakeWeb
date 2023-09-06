b6cbcc941ee7398f6855891b5d0f7debdca32a64
Miguel Vargas <migs.a.vargas@gmail.com>
2023-09-05 21:51:40 -0400

    Remove bootstrap gutters, add self gutters. Use dynamic ngFor to preview cards, responsive design.

--- src/my-take/features/dashboard/dashboard.component.ts
+++ src/my-take/features/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { TakeCardComponent } from '../../shared/components/take-card/take-card.component';
 import { DashboardSearchComponent } from './dashboard-search/dashboard-search.component';
 
 @Component({
-    selector: 'my-take-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss'],
-    standalone: true,
-    imports: [DashboardSearchComponent, TakeCardComponent],
+	selector: 'my-take-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.scss'],
+	standalone: true,
+	imports: [DashboardSearchComponent, TakeCardComponent, NgFor],
 })
 export class DashboardComponent {}
