7cbda8338c259e512caa4b810b3a50f2f3f909d5
Miguel Vargas <migs.a.vargas@gmail.com>
2023-09-18 23:39:03 -0400

    external toolbar goes to dashboard on login.

--- src/my-take/external/external-toolbar/external-toolbar.component.ts
+++ src/my-take/external/external-toolbar/external-toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 
@@ -8,6 +9,6 @@ import { RouterLink } from '@angular/router';
 	templateUrl: './external-toolbar.component.html',
 	styleUrls: ['./external-toolbar.component.scss'],
 	standalone: true,
-	imports: [MatToolbarModule, MatButtonModule, RouterLink],
+	imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
 })
 export class ExternalToolbarComponent {}
