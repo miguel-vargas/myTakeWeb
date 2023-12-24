2b24c1ac1089c9e2ad21e5e6a663cc8f0487ec7f
Miguel Vargas <migs.a.vargas@gmail.com>
2023-12-24 02:33:10 -0500

    rudimentary search.

--- src/my-take/core/components/search/search.component.ts
+++ src/my-take/core/components/search/search.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'my-take-search',
@@ -17,7 +18,22 @@ import { MatInputModule } from '@angular/material/input';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  @Output() onSearch = new EventEmitter<string>();
+
 	searchForm = new FormGroup({
 		searchTerm: new FormControl(''),
 	});
+
+  constructor() {
+    this.searchForm.controls.searchTerm.valueChanges
+      .pipe(
+        debounceTime(400),
+        distinctUntilChanged()
+      )
+      .subscribe(term => {
+        if (term !== null) {
+          this.onSearch.emit(term.toLowerCase());
+        }
+      });
+  }
 }
--- src/my-take/core/features/places/places.component.ts
+++ src/my-take/core/features/places/places.component.ts
@@ -2,7 +2,7 @@ import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { PlaceCardComponent } from '@core/components/place-card/place-card.component';
 import { SearchComponent } from '@core/components/search/search.component';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Place } from 'src/my-take/data/models/place';
 import { PlaceService } from 'src/my-take/data/services/place.service';
 
@@ -16,4 +16,15 @@ import { PlaceService } from 'src/my-take/data/services/place.service';
 export class PlacesComponent {
   placeService: PlaceService = inject(PlaceService);
   places$: Observable<Place[]> = this.placeService.getPlaces();
+
+  searchPlaces(searchTerm: string) {
+    if (searchTerm === '') {
+      this.places$ = this.placeService.getPlaces();
+    }
+    else {
+      this.places$ = this.places$.pipe(
+        map(places => places.filter(place => place.name.toLowerCase().includes(searchTerm) || place.description.toLowerCase().includes(searchTerm)))
+      );
+    }
+  }
 }
