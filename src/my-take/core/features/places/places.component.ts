7e9d59d525c656e1d907f3b079efe3dadbc63fd8
Miguel Vargas <migs.a.vargas@gmail.com>
2023-12-24 19:54:40 -0500

    Finish search logic.

--- src/my-take/core/features/places/places.component.ts
+++ src/my-take/core/features/places/places.component.ts
@@ -2,7 +2,7 @@ import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { PlaceCardComponent } from '@core/components/place-card/place-card.component';
 import { SearchComponent } from '@core/components/search/search.component';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
 import { Place } from 'src/my-take/data/models/place';
 import { PlaceService } from 'src/my-take/data/services/place.service';
 
@@ -15,16 +15,24 @@ import { PlaceService } from 'src/my-take/data/services/place.service';
 })
 export class PlacesComponent {
   placeService: PlaceService = inject(PlaceService);
-  places$: Observable<Place[]> = this.placeService.getPlaces();
+  searchTerm$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  places$: Observable<Place[]> = combineLatest([
+    this.searchTerm$,
+    this.placeService.getPlaces()
+  ])
+  .pipe(
+      map(([searchTerm, places]) => {
+        if(searchTerm) {
+          places.sort((a, _) => a.name.toLowerCase().startsWith(searchTerm) ? -1 : 1);
+        } else {
+          places.sort((a, b) => a.simpleId - b.simpleId);
+        }
+      
+        return places.filter(place => place.name.toLowerCase().includes(searchTerm) || place.description.toLowerCase().includes(searchTerm));
+      })
+  );
 
   searchPlaces(searchTerm: string) {
-    if (searchTerm === '') {
-      this.places$ = this.placeService.getPlaces();
-    }
-    else {
-      this.places$ = this.places$.pipe(
-        map(places => places.filter(place => place.name.toLowerCase().includes(searchTerm) || place.description.toLowerCase().includes(searchTerm)))
-      );
-    }
+    this.searchTerm$.next(searchTerm);
   }
 }
--- src/my-take/data/models/place.ts
+++ src/my-take/data/models/place.ts
@@ -2,6 +2,7 @@ import { Address } from "./address";
 
 export interface Place {
     id: string,
+    simpleId: number,
     name: string,
     description: string,
     address?: Address,
